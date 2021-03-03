4ae2432493b993415d851a5a2ada2343429b7f9d
Guille <Guille@mailcom>
2021-03-03 12:49:30 +0100

    Start

---
+++ app.R
@@ -0,0 +1,57 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+
+
+list_choices =  unique(msleep$vore)[!is.na(unique(msleep$vore))]
+names(list_choices) = paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
+col_scale <- scale_colour_discrete(limits = list_choices)
+
+# Define UI for application that draws a histogram
+ui = navbarPage('Shiny App',
+    tabPanel('msleep',
+        fluidPage(
+            sidebarLayout(
+                sidebarPanel(
+                    selectInput("select", label = h3("Plot by type of alimentation"), 
+                                choices = character(0),
+                                selected = 1)
+                ),
+                mainPanel(
+                    plotOutput(outputId = "plot")
+                )
+            )
+        )
+    ),
+    tabPanel("References",
+             includeMarkdown("references.md")
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+    
+    # Can also set the label and select items
+    updateSelectInput(session, "select",
+                      choices = list_choices,
+                      selected = tail(list_choices, 1))
+
+    output$plot = renderPlot({
+        if(input$select != ""){
+            ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
+                scale_x_log10() +
+                col_scale +
+                geom_point(size = 5)
+        }
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
